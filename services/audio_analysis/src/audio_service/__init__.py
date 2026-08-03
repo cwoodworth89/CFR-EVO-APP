6abbd34e3a363802e1822472dde995f739e1984d
cwoodworth89 <curtis.l.woodworth@gmail.com>
2026-08-03 16:33:27 -0700

    fix: export get_all_matches in audio_service package init

--- services/audio_analysis/src/audio_service/__init__.py
+++ services/audio_analysis/src/audio_service/__init__.py
@@ -2,6 +2,7 @@ from audio_service.dsp_tone_spotter import (
     get_rms,
     analyze_live_audio,
     get_best_match,
+    get_all_matches,
     filter_known_tones
 )
 from audio_service.sound_capture import capture_full_dispatch

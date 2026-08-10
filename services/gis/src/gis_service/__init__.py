3d3c620186a28a6e87f169edc7e850756d0c4aaa
cwoodworth89 <curtis.l.woodworth@gmail.com>
2026-08-09 23:01:40 -0700

    fix: make gis_service imports resilient for standalone routing engine

--- services/gis/src/gis_service/__init__.py
+++ services/gis/src/gis_service/__init__.py
@@ -1,2 +1,7 @@
-from gis_service.shapefile_loader import load_addresses, load_zones
-from gis_service.geocoder import CoquitlamDataValidator
+try:
+    from gis_service.shapefile_loader import load_addresses, load_zones
+    from gis_service.geocoder import CoquitlamDataValidator
+except ModuleNotFoundError:
+    pass
+
+from gis_service.routing_engine import EVORoutingEngine
